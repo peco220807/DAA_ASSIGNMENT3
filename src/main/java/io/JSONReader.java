5b228ff8fb524f05eb1be29d437f0be4ad04afa8
peco220807 <saparbai.s07@icloud.com>
2025-10-25 18:30:51 +0500

    feat(io): add JSON file reader and writer utilities

--- src/main/java/io/JSONReader.java
+++ src/main/java/io/JSONReader.java
@@ -1,4 +1,18 @@
 package io;
-public class JSONReader {
+import java.io.File;
+import java.io.IOException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+
+public class JSONReader {
+    public static <T> T readFromJson(String filename, Class<T> classType) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            T data = objectMapper.readValue(new File(filename), classType);
+            return data;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- src/main/java/io/JSONWriter.java
+++ src/main/java/io/JSONWriter.java
@@ -1,4 +1,19 @@
 package io;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+
 
 public class JSONWriter {
+
+    public static <T> void writeToJson(String filename, T data) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            objectMapper.writeValue(new File(filename), data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
