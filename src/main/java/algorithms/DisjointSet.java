d8349190afc4784dc330c646bc1684f010e06848
peco220807 <saparbai.s07@icloud.com>
2025-10-25 18:42:54 +0500

    implement DisjointSet data structure

---
+++ src/main/java/algorithms/DisjointSet.java
@@ -0,0 +1,38 @@
+package algorithms;
+public class DisjointSet {
+    private final int[] parent;
+    private final int[] rank;
+    public DisjointSet(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]); // компрессия пути
+        }
+        return parent[x];
+    }
+
+    public boolean union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+        if (rootA == rootB) return false; // уже соединены
+        if (rank[rootA] < rank[rootB]) {
+            parent[rootA] = rootB;
+        } else if (rank[rootA] > rank[rootB]) {
+            parent[rootB] = rootA;
+        } else {
+            parent[rootB] = rootA;
+            rank[rootA]++;
+        }
+        return true;
+    }
+    public boolean same(int a, int b) {
+        return find(a) == find(b);
+    }
+}
